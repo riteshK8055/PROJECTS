23e5a30b27525584b488c337adcce5450aabd33b
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-20 18:03:22 +0530

    task model updated

--- node_Backend_Projects/todo_App/models/task.models.js
+++ node_Backend_Projects/todo_App/models/task.models.js
@@ -2,29 +2,30 @@ import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
 
-    name:{
+    title:{
 
         type:String,
         required:true
     },
 
-    email:{
-
-        type:String,
-        required: true,
-        unique: true
-    },
-
-    password:{
+    description:{
 
         type:String,
         required:true,
-        select:false
+    },
+
+    isCompleted:{
+
+        type:Boolean,
+        default: false
+    },
+
+    user:{
+
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
     }
 },
-    
     {timestamps:true}
 );
-
-
-export const Task = mongoose.model("Task", taskSchema);
