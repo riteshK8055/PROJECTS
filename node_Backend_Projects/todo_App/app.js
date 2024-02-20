0970d800650e6b0f8cd20a414885ea83fb740fc1
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-20 17:41:47 +0530

    config path added

--- node_Backend_Projects/todo_App/app.js
+++ node_Backend_Projects/todo_App/app.js
@@ -3,11 +3,13 @@ import { config } from "dotenv";
 
 export const  app = express();
 
-config({
+config();
 
-    path: "./data/config.env",
-});
+// config({
+
+//     path: "./data/config.env",
+// });
 
 
 app.use(express.json());
-app.use("/users", userRouter);
+// app.use("/users", userRouter);
