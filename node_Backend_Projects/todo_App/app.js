3f318bc6bed552517a4746ca6abe93a22b474207
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-21 16:51:43 +0530

    mounted router at specified path

--- node_Backend_Projects/todo_App/app.js
+++ node_Backend_Projects/todo_App/app.js
@@ -1,15 +1,15 @@
 import express from "express";
 import { config } from "dotenv";
+import userRouter from "./routes/user.router.js"
 
 export const  app = express();
 
-config();
 
-// config({
+config({
 
-//     path: "./data/config.env",
-// });
+    path: "./data/config.env",
+});
 
 
 app.use(express.json());
-// app.use("/users", userRouter);
+ app.use("/api/v1/users", userRouter);
