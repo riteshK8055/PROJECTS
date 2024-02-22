9c2e3e0b9576c185552f3b431e0f728c423ecc99
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-22 15:34:47 +0530

    routes to get user detail aded

--- node_Backend_Projects/todo_App/routes/user.router.js
+++ node_Backend_Projects/todo_App/routes/user.router.js
@@ -1,12 +1,16 @@
 import express from "express";
 
-import { login, register } from "../controllers/users.js";
+import { getMyProfile, login, register } from "../controllers/users.js";
+
+import { isAuthenticated } from "../middlewares/auth.js";
 
  const router = express.Router();
 
 router.post("/login", login);
 
 router.post("/register",register);
+
+router.get("/me",isAuthenticated,getMyProfile);
 
 
 export default router;
