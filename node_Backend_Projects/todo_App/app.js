befc010ce0a1f93d1fdfe144ccc33f1bc4f73b73
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-23 21:37:02 +0530

    added express router for task route

--- node_Backend_Projects/todo_App/app.js
+++ node_Backend_Projects/todo_App/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
-import userRouter from "./routes/user.router.js"
+import taskRouter from "./routes/task.router.js";
+import userRouter from "./routes/user.router.js";
 import cookieParser from "cookie-parser";
 
 export const  app = express();
@@ -15,4 +16,6 @@ config({
 
 app.use(express.json());
 app.use(cookieParser());
- app.use("/api/v1/users", userRouter);
+
+ app.use("/api/v1/users", userRouter);  
+ app.use("/api/v1/task",taskRouter);
