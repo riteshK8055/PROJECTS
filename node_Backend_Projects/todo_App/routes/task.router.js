bc3f826b020528f578714bfaa97c0a2b0cf92b80
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-23 21:35:29 +0530

    addded route for task

---
+++ node_Backend_Projects/todo_App/routes/task.router.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { newTask } from "../controllers/task.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+
+
+const router = express.Router();
+
+router.post("/new",isAuthenticated, newTask);
+
+export default router;
