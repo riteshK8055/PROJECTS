cb53da5040d82821e5ff2349d3d4c652a97e15d9
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-23 12:03:52 +0530

    resolve some error in this foldeer

--- node_Backend_Projects/todo_App/middlewares/auth.js
+++ node_Backend_Projects/todo_App/middlewares/auth.js
@@ -1,13 +1,11 @@
-import { request } from "express";
+
 import { User } from "../models/users.models.js";
 import jwt from "jsonwebtoken";
 
 
 export const isAuthenticated = async(req,res,next)=>{
 
-    console.log(req.cookies);
     const {token} = req.cookies;
-
     if(!token) 
 
     return res.status(404).json({
