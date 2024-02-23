3ae5e79f9b79ec06ad2f3b5aa5890d848a89db28
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-23 21:34:46 +0530

    added logic for adding tasks

---
+++ node_Backend_Projects/todo_App/controllers/task.js
@@ -0,0 +1,23 @@
+
+import { Task } from "../models/task.models.js";
+
+
+export const newTask = async(req,res,next)=>{
+
+
+    const{title,description} = req.body;
+    
+    await Task.create({
+
+        title,
+        description,
+        user:req.user,
+    });
+
+    res.status(201).json({
+
+        success:true,
+        message:"Task Added Successfully",
+    });
+
+};
