15f6128bd77bc24c38a438ee877d48fe27151a65
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-25 15:39:03 +0530

    routes added for update and delete task

--- node_Backend_Projects/todo_App/routes/task.router.js
+++ node_Backend_Projects/todo_App/routes/task.router.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getMyTask, newTask } from "../controllers/task.js";
+import { getMyTask, newTask, updateTask,deleteTask} from "../controllers/task.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post("/new",isAuthenticated, newTask);
 
 router.get("/my",isAuthenticated,getMyTask);
+
+router.route("/:id").put( isAuthenticated , updateTask).delete( isAuthenticated , deleteTask);
 
 export default router;
