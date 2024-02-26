6396e211a4f9ea8828e08a5d5ca7708b1eaadf17
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-26 20:20:15 +0530

    added more properties in cookies for deployment

--- node_Backend_Projects/todo_App/controllers/users.js
+++ node_Backend_Projects/todo_App/controllers/users.js
@@ -81,7 +81,12 @@ export const getMyProfile = (req,res,next)=>{
 export const logout = (req,res,next) =>{
 
     res.status(200)
-    .cookie("token", "", {expires:new Date(Date.now()) })
+    .cookie("token", "", {expires:new Date(Date.now()),
+    
+        sameSite:process.env.NODE_ENV === "Development"? "lax" : "none",
+        secure:process.env.NODE_ENV === "Development"?false : true,
+    
+    })
     .json({
 
         success:true,
