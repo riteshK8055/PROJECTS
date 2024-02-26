17a75999bf685b0cb6a46ee617aad59a35ce820a
Ritesh Kumar <riteshk8055@gmail.com>
2024-02-26 20:22:13 +0530

    enable cors for deployment

--- node_Backend_Projects/todo_App/app.js
+++ node_Backend_Projects/todo_App/app.js
@@ -4,6 +4,7 @@ import taskRouter from "./routes/task.router.js";
 import userRouter from "./routes/user.router.js";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
 
 export const  app = express();
 
@@ -18,7 +19,16 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(cors({
+
+    origin:[process.env.FRONTEND_URL],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true,
+ })
+);
+
  app.use("/api/v1/users", userRouter);  
  app.use("/api/v1/task",taskRouter);
 
+ //Using Error Middleware
  app.use(errorMiddleware);
