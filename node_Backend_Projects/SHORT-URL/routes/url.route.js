c536076ff91625b3d548ff77bebfb0788285f0d3
Ritesh Kumar <riteshk8055@gmail.com>
2024-03-03 08:49:03 +0530

    added analytics route

--- node_Backend_Projects/SHORT-URL/routes/url.route.js
+++ node_Backend_Projects/SHORT-URL/routes/url.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { generateShortURL } from "../controllers/url.controller.js";
+import { generateShortURL, handleGetAnalytics } from "../controllers/url.controller.js";
 
 const router = express.Router();
 
 router.post('/',generateShortURL);
+
+router.get("/analytics/:shortId",handleGetAnalytics);
 
 export default router;
